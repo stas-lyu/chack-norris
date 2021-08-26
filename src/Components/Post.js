a7fbd6fc101de2bb394c65569b318ec395d70e27
stas-lyu <staslyu1997@gmail.com>
2021-08-26 14:02:51 +0300

    build version 1.0.0

--- src/Components/Post.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import chackImg from '../chack.png'
-
-export default function Post({text, state}) {
-    return (<div className={state ? 'post-container active' : 'post-container'}>
-        <img className={'post-img'} srcSet={chackImg} alt="Chack Norris"/>
-        <p className={'post-text'}>{text}</p>
-    </div>)
-}
